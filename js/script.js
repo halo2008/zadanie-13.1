c78ab27c892c21a56ee5e0667f7c5b3da5505928
Konrad Sedkowski <ksedkowski@gmail.com>
2018-08-22 17:01:30 +0200

    add script

---
+++ js/script.js
@@ -0,0 +1,65 @@
+var data = [
+	{
+		id: 'box1',
+		title: 'First box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted', 'special-header', 'important']
+	},
+	{
+		id: 'box2',
+		title: 'Second box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['special-header', 'important']
+	},
+	{
+		id: 'box3',
+		title: 'Third box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted', 'important']
+	},
+	{
+		id: 'box4',
+		title: 'Fourth box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted']
+	},
+	{
+		id: 'box5',
+		title: 'Fifth box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: []
+	},
+];
+
+/*var idOne = data[0].id;
+var addClass = data[0].categories;
+
+var element = document.getElementById('box1');
+    element.classList.add(data[0].categories);*/
+
+var boxes =	document.getElementById('box');
+var el; 
+for (var i=0; i < data.length; i++) {
+	
+	
+	//cat.replace(/,/g," ");
+
+	el = document.createElement('div');
+	el.setAttribute('id', data[i].id);
+
+	var header = document.createElement('h1');
+	var text = document.createTextNode(data[i].title);
+	header.appendChild(text);
+	el.appendChild(header);
+	boxes.appendChild(el);	
+	
+	
+	
+	para = document.createTextNode(data[i].content);	
+	
+	el.appendChild(para);
+
+	var cat = (data[i].categories).join("");
+	el.classList.add(cat);
+
+}
